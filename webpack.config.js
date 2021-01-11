8d80f170820917fec0ea36dd0dfe72585191e03e
Vitalie Melnic <vmwhoami@gmail.com>
2021-01-11 10:57:28 +0200

    add mode development to package.json file start action

--- webpack.config.js
+++ webpack.config.js
@@ -28,15 +28,6 @@ module.exports = {
         ],
       },
       {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
-      },
-      {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
