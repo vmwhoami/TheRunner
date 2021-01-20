d403f9001f7d2d2708d3d990519a0615a5e68b01
vmwhoami <vmwhoami@gmail.com>
2021-01-20 17:21:04 +0200

    add text for the leaderboard

--- src/scenes/LeaderScene.js
+++ src/scenes/LeaderScene.js
@@ -15,18 +15,45 @@ export default class LeaderScene extends Phaser.Scene {
 
   }
   create() {
-    let settingScore = async () => {
-      let response = await scoreGetter();
-      response.result.forEach(element => {
-        console.log(element.score);
-      });
-    }
-    settingScore()
     placeImg(this, 'leader', 0);
     const replay = new Button(this, "div", 'btn', "replay", 100);
     replay.addListener('click').on('click', () => {
       this.scene.start('GameScene');
     })
+
+    let settingScore = async () => {
+      this.add.text(350, 100, 'RANK  SCORE   NAME')
+      let response = await scoreGetter();
+      let sortedresult = response.result.sort((a, b) => (a.score > b.score ? -1 : 1));
+
+      let count = 0;
+      let position = 130;
+      sortedresult.forEach(result => {
+        console.log(result.user);
+        console.log(result.score);
+        count += 1;
+        let st;
+        if (count < 10) {
+          if (count === 1) {
+            st = 'st'
+          } else if (count === 2) {
+            st = 'nd'
+          } else if (count > 3) {
+            st = 'th'
+          } else {
+            st = 'd'
+
+          };
+
+          this.add.text(300, position, `    ${count} ${st} ${result.score}  ${result.user}`).setTint(0xffffff);
+          position += 25;
+        }
+
+
+      });
+    }
+    settingScore()
+
 
 
   }
